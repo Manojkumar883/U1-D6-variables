e4945e595db6cff8e929770640aa1b78cdb62b3f
Manojkumar883 <113252691+Manojkumar883@users.noreply.github.com>
2022-10-04 16:52:59 +0200

    variables-HW

---
+++ Hw-1/index.js
@@ -0,0 +1,19 @@
+// name and surname
+
+
+let firstName = "Manojkumar";
+let surName =  "Bhoosani";
+
+console.log ('Hello!, my name is:', firstName + ' ' + surName );
+// age
+let age;
+age= 28;
+
+// current year
+let currentYear;
+currentYear=2022;
+
+// birth of year
+
+console.log('BirthYear:', currentYear - age);
+
